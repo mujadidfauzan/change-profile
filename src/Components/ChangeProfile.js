2b808c6db8725e4f24e4989f27dac4b5356e91fd
Fauzan <kaptozora@gmail.com>
2023-05-26 20:01:08 +0700

    finished

--- src/Components/ChangeProfile.js
+++ src/Components/ChangeProfile.js
@@ -8,9 +8,11 @@ const ChangeProfile = () => {
       </div>
       <div class="custom-card-2 mx-auto mt-2"></div>
       <div class="d-flex flex-column justify-content-center mt-3 text-white fw-bold">
-        <div class="card mt-4 mx-2 d-flex align-items-center justify-content-center shadow-sm" style={{ height: '40px', backgroundColor: '#006c84' }}>
-          <span class="text-center">Ambil Dari Kamera</span>
-        </div>
+        <Link to="/galeri" style={{ textDecoration: 'none' }}>
+          <div class="card mt-4 mx-2 d-flex align-items-center justify-content-center shadow-sm" style={{ height: '40px', backgroundColor: '#006c84' }}>
+            <span class="text-center">Ambil Dari Kamera</span>
+          </div>
+        </Link>
         <Link to="/galeri" style={{ textDecoration: 'none' }}>
           <div class="card m-2 d-flex align-items-center justify-content-center shadow-sm" style={{ height: '40px', backgroundColor: '#006c84' }}>
             <span class="text-center text-white">Ambil Dari Gallery </span>
